69467347b878dc899624bbc726dcde1c7540558f
Md Tahmidul Abedin <aunik43@gmail.com>
2025-07-30 01:47:08 +0600

    initial commit

---
+++ src/webparts/feedbackForm/components/Readme.md
@@ -0,0 +1,14 @@
+### Package need to be added
+
+"bootstrap": "4.6.0",
+"@pnp/odata": "2.0.13",
+"@pnp/sp": "2.013",
+"react-icons": "4.10.1",
+
+### Build options
+
+gulp clean - TODO
+gulp test - TODO
+gulp serve - TODO
+gulp bundle - TODO
+gulp package-solution - TODO
---
+++ src/webparts/feedbackForm/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IFeedbackFormWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'FeedbackFormWebPartStrings' {
+  const strings: IFeedbackFormWebPartStrings;
+  export = strings;
+}
